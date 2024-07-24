741b54b37c7a9e8cbb65612b70f0840bafe507e4
Javier <javier.duarte.iti@gmail.com>
2024-07-24 11:05:32 -0500

    Añadiendo Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        /* ## Colors */
+        "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "desaturated-dark-blue": "hsl(214, 17%, 51%)",
+        "grayish-blue": "hsl(212, 23%, 69%)",
+        "light-grayish-blue": "hsl(210, 46%, 95%)",
+      },
+    },
+  },
+  plugins: [],
+};
